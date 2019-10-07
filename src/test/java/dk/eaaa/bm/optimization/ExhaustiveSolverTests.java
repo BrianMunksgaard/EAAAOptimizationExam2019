62b714f39b16779688a1e6601412f107ce48af16
BrianMunksgaard <brian@qampo.com>
2019-10-07 20:08:10 +0200

    Project setup.

---
+++ src/test/java/dk/eaaa/bm/optimization/ExhaustiveSolverTests.java
@@ -0,0 +1,43 @@
+//package dk.eaaa.bm.optimization;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.JUnit4;
+//import org.slf4j.event.Level;
+//
+//import dk.eaaa.bm.optimization.util.TimerUtil;
+//
+//
+//@RunWith(JUnit4.class)
+//public class ExhaustiveSolverTests {
+//
+//	private org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass().getName());	
+//		
+//	@Test
+//	public void testExhaustiveSolutionForP1() {
+//		TimerUtil tu = TimerUtil.start(log, Level.INFO, "P1 exhaustive test");
+//		Problem p = new P1();
+//		ExhaustiveSolver solver = new ExhaustiveSolver();
+//		solver.solve(p);
+//		tu.stop();
+//	}
+//	
+//
+//	@Test
+//	public void testExhaustiveSolutionForP2() {
+//		TimerUtil tu = TimerUtil.start(log, Level.INFO, "P2 exhaustive test");
+//		Problem p = new P2();
+//		ExhaustiveSolver solver = new ExhaustiveSolver();
+//		solver.solve(p);
+//		tu.stop();
+//	}
+//
+//	@Test
+//	public void testExhaustiveSolutionForRevAckley() {
+//		TimerUtil tu = TimerUtil.start(log, Level.INFO, "RevAckley exhaustive test");
+//		Problem p = new RevAckley();
+//		ExhaustiveSolver solver = new ExhaustiveSolver();
+//		solver.solve(p);
+//		tu.stop();
+//	}
+//}
---
+++ src/test/java/dk/eaaa/bm/optimization/ga/SimpleGeneticAlgorithmTests.java
@@ -0,0 +1,14 @@
+package dk.eaaa.bm.optimization.ga;
+
+import org.junit.Test;
+
+import dk.eaaa.bm.optimization.ga.SimpleGeneticAlgorithm;
+
+public class SimpleGeneticAlgorithmTests {
+
+	@Test
+	public void runAlgo() {
+		SimpleGeneticAlgorithm ga = new SimpleGeneticAlgorithm();
+		ga.runAlgorithm(50, "1011000100000100010000100000100111001000000100000100000000001111");
+	}
+}
